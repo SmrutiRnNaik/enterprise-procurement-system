e3188571c8636438c793e68e17d688265d73b58a
Smruti Ranjan Naik <smrutiranjannaik5@gmail.com>
2026-07-29 02:22:29 +0530

    Initial Spring Boot project setup

---
+++ src/test/java/com/infosys/procurement/EnterpriseProcurementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infosys.procurement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnterpriseProcurementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
